6fe2fb848c5aa4927d156836722ad37aa3198df4
Quartzs_Book <progquartz@gmail.com>
2022-11-14 01:00:52 +0900

    맵제너레이팅 #02 - 맵 이동 구현 Done - 맵 생성 구현, 맵 데이터 존재할 경우 유형에 따른 맵 인카운터가 가능하도록 설정.
    Todo
    - 맵 유형을 받아와 이에 따른 색깔변화 가능하도록 만들기
    - 데이터 로드 / 세이브를 json 파일로 가능하도록 만들기

--- BelowAbyss/Assets/Scripts/System/MapSystem/MapData.cs
+++ BelowAbyss/Assets/Scripts/System/MapSystem/MapData.cs
@@ -8,15 +8,108 @@ public class MapData : MonoBehaviour
     /// MapData는 기본적으로 맵의 데이터를 저장하도록 만들어진 클래스이다.
     /// 
     /// 저장되어야 할 목록들은 일반적으로 다음과 같다.
-    /// - 모든 맵 내의 데이터들 (경로 유형, 이벤트 유형, 다음 방 유형)
+    /// - 정해진 맵 내의 데이터들 (경로 유형, 이벤트 유형, 다음 방 유형)
     /// - 맵에 추가적으로 적용되어야 하는 멀티플라이어들. // bool값으로 저장되어, 이들을 MapManager에서 관리하여 처리될 예정.
     /// - 매 이동(경로, 이벤트)에서 소비되어야 할 수치.
+    /// - 플레이어의 위치.
     /// 
     /// </summary>
 
-    private void Start()
+    // valid하는지 매번 체크함.
+    private bool IsMapdataValid = false;
+
+    // 맵 내에서의 플레이어 데이터.
+    // 경로와 이벤트 유형, 그리고 방까지 해서 다 합하는 것으로 진행.
+    // 첫 방은 0 -> 0
+    // 경로는 1,2, 4,5, 7,8, 10,11, 13,14 -> 3으로 나눌 시 나머지가 1,2인 수.
+    // 이벤트는 3,6,9,12 -> 3으로 나눠지는 수. (0제외)
+    // 마지막 방은 15 -> 15.
+    [SerializeField]
+    private int playerPos;
+    // 맵 내의 데이터.
+    private List<bool> pathsVisited; // 나중에 기획자료 제대로 도착하면 int대신 enum으로 수정하기. paths는 혹시 몰라서 
+
+    private List<int> events;
+    private List<bool> eventVisited;
+
+    private int room; // 출발하는 이전 방도 들어갈 수 있다면, List<int>일 것이고, 아니면 그냥 int일것.
+    private bool roomVisited;
+
+    public void MapFirstSetup(int stagenum)
     {
-        
+
+        int randomnum;
+        // Paths에 새로운 데이터 입력. 나중에 계수와 수식이 붙으면 다른 함수로 분리될 수도 있음.
+        pathsVisited = new List<bool>();
+        for (int i = 0; i < 10; i++)
+        {
+            pathsVisited.Add(false);
+        }
+
+        // Events에 새로운 데이터 입력.나중에 계수와 수식이 붙으면 다른 함수로 분리될 수도 있음.
+        events = new List<int>();
+        eventVisited = new List<bool>();
+        for (int i = 0; i < 4; i++)
+        {
+            randomnum = 0; // random한 코드가 들어갈 예정. 
+            events.Add(randomnum);
+            eventVisited.Add(false);
+        }
+
+        randomnum = 0;
+        room = randomnum;
+        roomVisited = false;
+
+    }
+
+    /// <summary>
+    /// 현재 있는 방의 이벤트 코드를 불러와 MapManager에게 알리는 함수.
+    /// </summary>
+    /// <returns></returns>
+    public int Encounter()
+    {
+        // 다음 방의 이벤트 호출.
+        if(playerPos == 15)
+        {
+            return room;
+        }
+        // 현재 있는 이벤트위치의 이벤트 호출.
+        else
+        {
+            return events[(playerPos / 3) - 1];
+        }
+    }
+
+    public bool IsFirstEncounter()
+    {
+        if(playerPos == 15)
+        {
+            return roomVisited;
+        }
+        if(playerPos == 0)
+        {
+            return true;
+        }
+        if(playerPos % 3 == 0)
+        {
+            // 이벤트는 3,6,9,12 -> 3으로 나눠지는 수. (0제외)
+            return eventVisited[(playerPos / 3) - 1];   
+        }
+        else
+        {
+            // 경로는 1,2, 4,5, 7,8, 10,11, 13,14 -> 3으로 나눌 시 나머지가 1,2인 수.
+            //       0,1, 2,3  4,5,  6,7    8, 9  
+            int add = 0;
+            if(playerPos % 3 == 1)
+            {
+                add = 0;
+            }
+            else if(playerPos % 3 == 2)
+            {
+                add = 1;
+            }
+            return pathsVisited[(2 * (playerPos / 3)) + add];
+        }
     }
 
     /// <summary>
@@ -25,7 +118,81 @@ public class MapData : MonoBehaviour
     /// <returns></returns>
     public bool CheckValidation()
     {
-        return true;
+        return IsMapdataValid;
     }
+
+    public int GetPosition()
+    {
+        return playerPos;
+    }
+
+    public void SetPosition(int position)
+    {
+        if (position > 15)
+        {
+            position = 15;
+        }
+        if (position < 0)
+        {
+            position = 0;
+        }
+        playerPos = position;
+        VisitPosition(position);
+    }
+
+    public void VisitPosition(int position)
+    {
+        if(position == 0)
+        {
+            return;
+        }
+        if (position == 15)
+        {
+            roomVisited = true;
+            return;
+        }
+        // 이벤트의 경우.
+        if (position % 3 == 0)
+        {
+            eventVisited[position / 3 - 1] = true;
+            return;
+        }
+        if (position % 3 == 1)
+        {
+            pathsVisited[(2 * (position / 3))] = true;
+            return;
+        }
+        if (position % 3 == 2)
+        {
+            pathsVisited[(2 * (position / 3)) + 1] = true;
+            return;
+        }
+    }
+
+    public int GetEvent(int index)
+    {
+        return events[index];
+    }
+
+    public bool GetEventVisited(int index)
+    {
+        return eventVisited[index];
+    }
+
+    public bool GetPathVisited(int index)
+    {
+        return pathsVisited[index];
+    }
+
+    public bool GetRoomVisited()
+    {
+        return roomVisited;
+    }
+
+    public int GetRoom()
+    {
+        return room;
+    }
+
 }
 
--- BelowAbyss/Assets/Scripts/System/MapSystem/MapManager.cs
+++ BelowAbyss/Assets/Scripts/System/MapSystem/MapManager.cs
@@ -9,13 +9,25 @@ using UnityEngine;
 /// 만약 MapData 내부의 데이터가 존재하게 된다면, MapManger는 이를 기반으로 Map에 데이터를 넣어 적용하도록 한다.
 /// 이는 각각에 존재하는 PathNode와 EventNode가 수행하게 된다.
 /// 
+/// 만약에 게임 매니저가 새로운 스테이지를 만들어라는 명령을 내리게 된다면, 이를 진행하면 된다.
+/// 
 /// </summary>
 public class MapManager : MonoBehaviour
 {
-
     private static MapManager instance;
 
+    [SerializeField]
     private MapData mapData;
+    [SerializeField]
+    private MapVisual MapVisual;
+    [SerializeField]
+    private int stageNum;
+
+    // 맵에 추가적으로 적용되어야 하는 멀티플라이어.
+    private float stageMultiplier; // 스테이지 증폭률. 나중에 가면 체력 / 공격력 등의 추가 보정으로 나뉠 예정.
+    private bool isElite; // 엘리트여부? 존재할지도?
+
+    private float supplyOutRate; // 매 이동마다 보급품 날아가는 비율.
 
     void Awake()
     {
@@ -49,15 +61,9 @@ public class MapManager : MonoBehaviour
 
     private void Start()
     {
-        if (CheckMapDataValid())
-        {
-            Debug.Log("맵 정당성이 확인됨.");
-        }
-        else
-        {
-            Debug.Log("맵 정당성이 확인되지 못함.");
-            Debug.Log("새 맵 데이터 생성중.");
-        }
+        stageNum = -1;
+        mapData = null;
+        MapVisual = GetComponent<MapVisual>();
     }
 
     /// <summary>
@@ -83,23 +89,31 @@ public class MapManager : MonoBehaviour
         }
     }
 
-    private void MapGeneration()
+    public void MapGeneration(int genStageNum)
     {
+        this.gameObject.AddComponent<MapData>();
+        mapData = this.GetComponent<MapData>();
+        MapVisual.mapdata = mapData;
+        stageNum = genStageNum;
+        mapData.MapFirstSetup(stageNum);
+    }
 
+    // 맵 생성, 맵 내 이동-> 내부데이터는 mapdata에서 처리, 맵 보여주기(비주얼) -> 실제는 mapvisual에서 처리.
+
+    public void Move(int position)
+    {
+        mapData.SetPosition((mapData.GetPosition() + position));
+        MapVisual.UpdateVisual();
+    }
+
+
+    public void MoveTo(int position)
+    {
+        mapData.SetPosition(position);
+    }
+
+    private void DeleteMap()
+    {
     }
     
-}
-
-enum MapEventType
-{
-
-}
-public class Path : MonoBehaviour
-{
-
-}
-
-public class Event : MonoBehaviour
-{
-
 }
